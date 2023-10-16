e779220c763e88874138f7fb047127785efdebe7
LouspDan <esaie.lupepele@gmail.com>
2023-10-16 04:19:07 -0700

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/* 4-puts.c by Ésaïe Daniel */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
